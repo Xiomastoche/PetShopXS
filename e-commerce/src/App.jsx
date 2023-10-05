20bec7d4dc1f37d6f0530b920c75e4787564685a
Xiomara <xioma.stochetta@hotmail.com>
2023-10-04 23:58:22 -0300

    CreaTuLanding+Stochetta

---
+++ e-commerce/src/App.jsx
@@ -0,0 +1,15 @@
+import './style.css';
+import NavBar from './components/NavBar/NavBar';
+
+
+function App() {
+
+  return (
+    <>
+      <NavBar></NavBar>
+      <container></container>
+    </>
+  )
+}
+
+export default App;
