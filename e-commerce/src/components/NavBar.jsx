413ba3a6b6c3b5901562ffbc87a9e17f44c19d64
Xiomara <xioma.stochetta@hotmail.com>
2023-10-06 14:05:00 -0300

    navbar + linda

--- e-commerce/src/components/NavBar.jsx
+++ e-commerce/src/components/NavBar.jsx
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import CartWidget from './CartWidget';
 
 import logo from '../img/logoveg.png';
 import carrito from '../img/shop.png';
@@ -16,27 +17,27 @@ const NavBar = () => {
         <Container fluid>
         <Navbar.Brand href="#home">
             <img
-              alt=""
+              alt="logo"
               src={logo}
               width="100"
-              className="d-inline-block align-top"
+              className="d-inline-bl ock mx-2"
             />
         </Navbar.Brand>
-        <Nav className="justify-content-center fw-bolder">
-            <Nav.Link href="#">Inicio</Nav.Link>
-            <Nav.Link href="#">Productos</Nav.Link>
-            <Nav.Link href="#">Contacto</Nav.Link>
-        </Nav>
-        <Nav className="justify-content-end">
+        <Nav className="justify-content-end fw-bolder" id="responsive-navbar-nav">
+          <Nav.Link className='px-3' href="#">Inicio</Nav.Link>
+          <Nav.Link className='px-3' href="#">Productos</Nav.Link>
+          <Nav.Link className='px-3' href="#">Contacto</Nav.Link>
+          <Nav className='px-3 border-start border-dark'>
             <a href="#">
             <img
-              alt=""
+              alt="carrito"
               src={carrito}
               width="40"
               className="img-fluid"
             />
             </a>
-            
+            <CartWidget />
+          </Nav>
         </Nav>
         </Container>
       </Navbar>
